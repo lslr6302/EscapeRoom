cdf892ddf5be761128d0c09b859e4f1b8650dd29
LorAc <89266230+lslr6302@users.noreply.github.com>
2023-06-27 23:06:33 -0400

    Update EscapeRoom.java

--- escapeRoom/EscapeRoom.java
+++ escapeRoom/EscapeRoom.java
@@ -28,11 +28,10 @@ public class EscapeRoom {
 		new EscapeRoom();
 	}
 	
-	GraphicsConsole gc = new GraphicsConsole(967, 747, "FISH!");
+	GraphicsConsole gc = new GraphicsConsole(967, 747);
 	BufferedImage backImg, playerImg;
 	Rectangle door, drawing, tv, closet, drawer, carpet;
-	int difficulty;
-	boolean clearScreen = false, didPong = false, didSudoku = false, didShooting = false, win = false;
+	boolean clearScreen = false, didPong = false, didSudoku = false, didShooting = true, win = false;
 	int playerX = 500, playerY = 600;
 	
 	EscapeRoom() {
@@ -55,13 +54,30 @@ public class EscapeRoom {
 	
 	//setup page for the starting page
 	void start() {
-		gc.setTitle("Testing Starting Page");
+		gc.setTitle("Welcome!");
 		gc.setLocationRelativeTo(null);
 		gc.setAntiAlias(true);
-		gc.enableMouse();  //enables mouse clicking
+		gc.enableMouse();
+		gc.setFont(new Font("Arial", Font.PLAIN, 20));
 		
 		gc.setBackgroundColor(Color.decode("#CCDDFF"));
 		gc.clear();
+		
+		//descriptions
+		String[] instructions = {"You have entered the Spooks Escape Room.",
+		"- Click in the room to find where the hidden games are to win the exit key for the escape room.",
+		"- You are only able to play each game once or else you have lost :(",
+		"- There will be 3 hidden games",
+		"\t- Pong: after one player got 3 points, the game will end.",
+		"\t- Sudoku: fill in all the blanks with 1-9, no repeated numbers are allowed in small grid, row or a column",
+		"\t- Shooting game: shoot down 10 balls within 20 shots.",
+		"- Good Luck!!"};
+		
+		for (int i = 0; i < instructions.length; i++) {
+			 gc.drawString(instructions[i], 20, 100+40*i);
+		}
+		
+		gc.drawString("--click anywhere to start--", 325, 500);
 	}
 	
 	//setup page for the main game
@@ -70,6 +86,7 @@ public class EscapeRoom {
 		gc.setLocationRelativeTo(null);
 		gc.setAntiAlias(true);
 		gc.enableMouse();
+		gc.clear();
 		
 		drawRect();
 	}
@@ -87,8 +104,6 @@ public class EscapeRoom {
 		int mx = gc.getMouseX();
 		int my = gc.getMouseY();
 		
-		String s = "dont close";
-		
 		if (door.contains(mx, my)) if (checkWin()) win();
 		if (drawing.contains(mx, my)) {
 			if (!didSudoku) {
@@ -99,13 +114,11 @@ public class EscapeRoom {
 		}
 		if (tv.contains(mx, my)) {
 			if (!didPong) {
-				new Pong(s);
+				Pong.main(null);
 				didPong = true;
 				return;
 			}
 		}
-		if (closet.contains(mx, my)) gc.showDialog("There is nothing hiding here.", "Nice Try");
-		if (drawer.contains(mx, my)) gc.showDialog("There is nothing hiding here.", "Nice Try");
 		if (carpet.contains(mx, my)) {
 			if (!didShooting) {
 				ShootingGame.main(null);
@@ -175,41 +188,3 @@ public class EscapeRoom {
 	}
 
 }
-
-/*
-Door
-	top-left:  504, 119
-	top-right： 720, 119
-	btm-left:  503, 586
-	btm-right: 720, 586
-
-Picture on the shelf
-	top-left:  275, 48
-	top-right： 431, 48
-	btm-left:  275, 163
-	btm-right: 431, 163
-	
-TV
-	top-left:  215, 315
-	top-right： 480, 315
-	btm-left:  215, 454
-	btm-right: 480, 454
-	
-Closet
-	top-left:  765, 55
-	top-right： 860, 100
-	btm-left:  765, 604
-	btm-right: 860, 664
-	
-Drawer
-	top-left:  43, 584
-	top-right： 172, 527
-	btm-left:  43, 684
-	btm-right: 172, 624
-	
-Carpet
-	top-left:  535, 652
-	top-right： 792, 652
-	btm-left:  635, 732
-	btm-right: 910, 732
-*/
